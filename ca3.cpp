f25d34146708a50e58eb121b6ce9bee3a8a03abf
Farid <faridghafooriadl@gmail.com>
2018-05-05 15:34:32 +0430

    read weight complete

--- ca3.cpp
+++ ca3.cpp
@@ -7,10 +7,17 @@
 #include <sstream>
 #include <fstream>
 
+using namespace std;
+
 #define INPUT_FILE "inputs.txt"
 #define WEIGHT_FILE "weights.txt"
 
-using namespace std;
+struct Values {
+	int BIAS;
+	vector<int> WEIGHTS;
+	vector<vector<int> > INPUTS;
+};
+
 
 vector<string> split_string(const string &s, char delim) {
   stringstream ss(s);
@@ -52,21 +59,55 @@ vector<vector<int> > input_parser(vector<string> store_input) {
 
 
 	// INPUT CHECK 
+	// cout << "INPUTS : \n\n";
 	// for(int i = 0; i < int_inputs_table.size(); i++) {
+	// 	cout << "Line number [ " << i+1 << " ] : \n"; 
 	// 	for(int j = 0; j < int_inputs_table[i].size(); j++) {
-	// 		cout << int_inputs_table[i][j] << " | ";
+	// 		cout << int_inputs_table[i][j] << " ";
 	// 	}
 	// 	cout << "\n\n";
 	// }
+	// cout << "--------------------------------------------\n";
 
 	return int_inputs_table;
 }
 
-void *read_inputs(void *arg) {
+vector<int> weight_parser(vector<string> store_weight) {
+	vector<int> int_weight_values;
+	vector<string> weights;
+	string a = store_weight[1].substr(1, store_weight[1].size()-2);
+	weights = split_string(a, ',');
+	for(int i = 0; i < weights.size(); i++) {
+		int_weight_values.push_back(string_to_int(weights[i]));
+	}
+
+
+	// CHECK WEIGHTS
+	// cout << "WIGHTTS : \n\n";
+	// for(int i = 0; i < int_weight_values.size(); i++) {
+	// 	cout << int_weight_values[i] << " ";
+	// }
+	// cout << endl;
+	// cout << "--------------------------------------------\n";
+
+
+	return int_weight_values;
+}
+
+int bias_parser(string bias_value) {
+	int bias;
+	string a = bias_value.substr(6, bias_value.size());
+	return bias = string_to_int(a);
+}
+
+void *read_inputs(void* arg) {
 	ifstream input_file;
 	string line;
 	vector<string> store_input;
 	vector<vector<int> > number_inputs;
+
+	// struct Values *my_value = (struct Values*)values;
+
 
 	input_file.open(INPUT_FILE);
 	if (!input_file) {
@@ -79,27 +120,22 @@ void *read_inputs(void *arg) {
 
 	number_inputs = input_parser(store_input);
 
+	// my_value->INPUTS = number_inputs;
+
 	input_file.close();
 	 
-  pthread_exit(NULL);
-}
-
-vector<int> weight_parser(vector<string> store_weight) {
-	vector<int> temp;
-	cout << store_weight[1] <<endl;
-	string a = store_weight[2].substr(1, store_weight[2].size()-2);
-	cout << a << endl;
-	return temp;
+ 	pthread_exit(NULL);
 }
 
 void *read_weights(void *arg) {
 	ifstream weight_file;
 	string line;
 	vector<string> store_weight;
-	int BIAS = 0;
+	int bias;
 	vector<int> weights;
 
 	weight_file.open(WEIGHT_FILE);
+	
 	if (!weight_file) {
     cerr << "Unable to open file " << WEIGHT_FILE << endl;
     exit(1);
@@ -111,28 +147,50 @@ void *read_weights(void *arg) {
 
 	weights = weight_parser(store_weight);
 
+	bias = bias_parser(store_weight[2]);
 
-	for(int i = 0; i < store_weight.size(); i++) {
-		cout << store_weight[i] << endl;
-	}
+	// WEIGHTS = weights;
+	// BIAS = bias;
+
+	// cout << "bias : ";
+	// cout << bias << endl;
+	// cout << "--------------------------------------------\n";
+
 
 	weight_file.close();
+  // cout << BIAS << endl;
 
-  pthread_exit(NULL);	
+	pthread_exit(NULL);	
 }
 
-int main()
-{
+void *middle_threads(void *arg) {
+
+}
+
+int main(int argc, char const *argv[]) {
   pthread_t inputs_thread, weights_thread;
+  struct Values *values;
+  // values = malloc(sizeof(struct *Values));
+  // int n;
+  // cout << "Please enter your middle threads : ";
+  // cin >> n;
+  // pthread_t middle_threads [n];
   int inputs_thread_err, weights_thread_err;
 
   inputs_thread_err = pthread_create(&inputs_thread, NULL, read_inputs, NULL);
   weights_thread_err = pthread_create(&weights_thread, NULL, read_weights, NULL);
 
+
   if(inputs_thread_err != 0 || weights_thread_err != 0) {
-	  cout << "Error: inputs_thread() or weights_thread() failed\n";
-	  exit(EXIT_FAILURE);
+		cout << "Error: inputs_thread() or weights_thread() failed\n";
+		exit(EXIT_FAILURE);
 	}
+
+
+
+	// for(int i = 0; i < n; i++) {
+		// pthread_create(&middle_threads[i], middle_threads, NULL);
+	// }
 
   pthread_exit(NULL);
 }
